145bbfd6f2b68060b1942a8ae6ea07bb3263dc87
nicoBlues <nicoxblues86@gmail.com>
2018-05-16 03:58:01 -0300

    update
    se agrega un pequeño soporte para sesiones

--- mux.go
+++ mux.go
@@ -7,9 +7,14 @@ import (
 	"net"
 	"net/http"
 	"strings"
+
+	"github.com/gorilla/sessions"
 )
 
 var multiPlex *multiplexor
+
+
+
 
 type Entity interface {
 	WriteEntity(*ClientCustomContext)
@@ -21,6 +26,7 @@ type parsedClientRequest struct {
 	Entity        *Entity
 	RawUrl        string
 	UrlParameters map[string][]string
+	clientSession *AppSession
 
 	ClientIP string
 }
@@ -28,6 +34,7 @@ type parsedClientRequest struct {
 type ClientCustomContext struct {
 	Ctx        *gin.Context
 	CliRequest *parsedClientRequest
+	OriginalClientRequest *http.Request
 }
 
 type HandlerMethod func(relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes
@@ -49,28 +56,51 @@ func (hc *handlerComunitaction) getDataContext(ctx *ClientCustomContext) interfa
 
 type InterpreterExecuteFunction func(*ClientCustomContext)
 
+
+func (hc *handlerComunitaction) getSessionFromRequest(ctx *ClientCustomContext) *AppSession{
+
+	session , _ := Store.Get(ctx.OriginalClientRequest,"sessionStore")
+
+	return  &AppSession{session,"sessionID"}
+
+
+}
+
 func (hc *handlerComunitaction) executeInterpreter(relativePath string, funcExec InterpreterExecuteFunction) {
 
 	hc.hadlerMethodRef(relativePath, func(context *gin.Context) {
 
+
+
 		hc.OriginalCtx = context
 		customContext := &ClientCustomContext{Ctx: context}
+		customContext.OriginalClientRequest = context.Request
 
-		clientWrapperReq := parsedClientRequest{}
-		customContext.CliRequest = &clientWrapperReq
+
+		appSession := hc.getSessionFromRequest(customContext)
 
 		ip, _ := getClientIPByRequest(context.Request)
+		clientWrapperReq :=
+			&parsedClientRequest{clientSession:appSession,
+												UrlParameters:context.Request.URL.Query(),
+												ClientIP:ip,
+												RawUrl:context.Request.URL.Path}
 
-		clientWrapperReq.ClientIP = ip
-		clientWrapperReq.RawUrl = context.Request.URL.Path
-		clientWrapperReq.UrlParameters = context.Request.URL.Query()
+
+		customContext.CliRequest = clientWrapperReq
 
 
+
+		var valStr string = "[ '{{repeat(5, 7)}}', { _id: '{{objectId()}}', index: '{{index()}}', guid: '{{guid()}}', isActive: '{{bool()}}', balance: '{{floating(1000, 4000, 2, ', picture: 'http://placehold.it/32x32', age: '{{integer(20, 40)}}', eyeColor: '{{random('blue', 'brown', 'green')}}', name: '{{firstName()}} {{surname()}}', gender: '{{gender()}}', company: '{{company().toUpperCase()}}', email: '{{email()}}', phone: '+1 {{phone()}}', address: '{{integer(100, 999)}} {{street()}}, {{city()}}, {{state()}}, {{integer(100, 10000)}}', about: '{{lorem(1, 'paragraphs')}}', registered: '{{date(new Date(2014, 0, 1), new Date(), 'YYYY-MM-ddThh:mm:ss Z')}}', latitude: '{{floating(-90.000001, 90)}}', longitude: '{{floating(-180.000001, 180)}}', tags: [ '{{repeat(7)}}', '{{lorem(1, 'words')}}' ], friends: [ '{{repeat(3)}}', { id: '{{index()}}', name: '{{firstName()}} {{surname()}}' } ], greeting: function (tags) { return 'Hello, ' + this.name + '! You have ' + tags.integer(1, 10) + ' unread messages.'; }, favoriteFruit: function (tags) { var fruits = ['apple', 'banana', 'strawberry']; return fruits[tags.integer(0, fruits.length - 1)]; } } ]"
+
+		appSession.Values["codigoRojo"] = []byte(valStr)
 
 		hc.getDataContext(customContext)
 
 
 		funcExec(customContext)
+
+		appSession.Save(customContext)
 
 	})
 
@@ -111,8 +141,15 @@ func getClientIPByRequest(req *http.Request) (ip string, err error) {
 	return userIP.String(), nil
 
 }
-
+var Store sessions.Store
 func NewMux() *multiplexor {
+
+
+	 Store = NewStoreForSessionType("asd",[]byte("super-secret_ohsi_key_megaHardcodeada"))
+	//Store  = NewCookieStore([]byte("super-secret_ohsi_key_megaHardcodeada"))
+
+
+
 
 	multiPlex = new(multiplexor)
 	r := gin.Default()
@@ -176,6 +213,7 @@ func (multi *multiplexor) AddMethodRestFul(methodName string, relativePath strin
 		//wrap.getMethodHandler()
 		wrap.executeInterpreter(relativePath, func(context *ClientCustomContext) {
 			log.Println("Interpreter ejecutado con exito ! ")
+
 
 			fMethod(context)
 
